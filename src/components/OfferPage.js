d3fb4db4322206b41e7b6c5b482b36f927df9a97
CodeWarrier <CodeWarrier3000@gmail.com>
2022-03-03 14:34:38 +0900

    updated

--- src/components/OfferPage.js
+++ src/components/OfferPage.js
@@ -1,26 +1,35 @@
 import { ethers } from 'ethers';
+import { formatEther } from 'ethers/lib/utils';
 import React, { useEffect, useState } from 'react';
 import { address } from '../constants/addresses';
 import { network } from '../constants/network';
 import ERC721 from '../contracts/ERC721A/ERC721AMock.sol/ERC721AMock.json';
+import ERC20 from '../contracts/TestERC20.sol/TestERC20.json';
 import TokenCard from './common/TokenCard';
 
-const OfferPage = ({account}) => {
-  const [ids, setIds] = useState([]);
+const OfferPage = ({ account }) => {
+  const [tokens, setTokens] = useState([]);
+  const [balance, setBalance] = useState();
   useEffect(() => {
     async function getCollection() {
-      try{
+      try {
         const provider = new ethers.providers.JsonRpcProvider(network.rpcUrls[0]);
         const contract = new ethers.Contract(address['erc721A'], ERC721.abi, provider);
+        if (!!account) {
+          const erc20 = new ethers.Contract(address['erc20'], ERC20.abi, provider);
+          const balance20 = await erc20.balanceOf(account);
+          setBalance(formatEther(balance20));
+        }
         const totalSupply = await contract.totalSupply();
         let tokenIdList = [];
-        for(let i = 0 ; i < Number(totalSupply) ; i++) {
+        for (let i = 0; i < Number(totalSupply); i++) {
           const owner = await contract.ownerOf(i);
-          if(owner.toLowerCase() === String(account).toLowerCase()) {
-            tokenIdList.push(i);
-          }
+          tokenIdList.push({
+            id: i,
+            owner: owner
+          });
         }
-        setIds(tokenIdList);
+        setTokens(tokenIdList);
       } catch (err) {
         console.log(err);
       }
@@ -29,13 +38,28 @@ const OfferPage = ({account}) => {
     getCollection();
   }, [account]);
   return (
-    <div className="grid grid-cols-4 gap-4 p-5">
-      {
-        ids.map(id => (
-          <TokenCard id={id} key={id} />
-        ))
-      }
+    <div>
+      <div className="grid grid-cols-4 gap-4 p-5">
+        {
+          tokens.map(token => (
+            <TokenCard id={token.id} key={token.id} owner={token.owner} account={account} />
+          ))
+        }
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        <div className=""></div>
+        <div className="bg-indigo-400 border border-white rounded-md w-full text-white p-5 grid grid-cols-2 gap-4">
+          <div>ERC20</div>
+          <div></div>
+          <div>Balance</div>
+          <div>{balance}</div>
+          <div>NFT ID</div>
+          <div></div>
+        </div>
+        <div></div>
+      </div>
     </div>
+
   )
 }
 
